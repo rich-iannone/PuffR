d66d695e33acb73b2802e3a8d9280cc9ce23a04b
rich-iannone <riannone@me.com>
2013-10-11 06:52:17 -0700

    Added grid definitions.

--- PuffR.R
+++ PuffR.R
@@ -12,6 +12,8 @@ install.packages("sp")
 library("sp")
 install.packages("rgdal")
 library("rgdal")
+install.packages("plyr")
+library("plyr")
 
 # Start with information on location for CALMET domain
 # Center of domain? LL? UR?
@@ -23,9 +25,6 @@ lat_dec_deg <- 49.062145
 long_dec_deg <- -123.128404
 lat_long_dec_deg <- cbind(long_dec_deg, lat_dec_deg)
 
-# Conversion to UTM should occur; first determine UTM zone
-UTM_zone <- (floor((long_dec_deg + 180)/6) %% 60) + 1
-
 # Generate a PROJ.4 string from an acceptable EPSG code
 # Include a dataframe that contains all acceptable EPSG codes for filtering
 # Information on this page: http://cicero.azavea.com/docs/epsg_codes.html
@@ -35,7 +34,7 @@ UTM_zone <- (floor((long_dec_deg + 180)/6) %% 60) + 1
 # +init=epsg:32610 +proj=utm +zone=10 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 
 
 projection <- c("utm")
-UTM_zone
+UTM_zone <- (floor((long_dec_deg + 180)/6) %% 60) + 1
 datum <- c("WGS84")
 units <- c("m")
 
@@ -43,20 +42,21 @@ units <- c("m")
 EPSG_code <- 32610
 proj_string <- c("+init=epsg:32610 +proj=utm +zone=10 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0")
 
-# Project as UTM coordinate
+# Project as UTM coordinate, round to nearest 50 m using round_any function ('plyr' package)
 UTM_location <- project(lat_long_dec_deg, proj_string)
+UTM_location <- round_any(UTM_location, 50, round)
 
 # Where is this point located on the grid?
-# Choices are: C (center), LL, LR, UL, UR
-lat_long_grid_loc <- c("C")
+# Choices are: 1 (center), 2 (lower left), 3 (lower right), 4 (upper left), 5 (upper right)
+lat_long_grid_loc <- 1
 
 # what is the the cell resolution (square cells) required (in meters)?
 cell_resolution_m <- 50
 
 # What is the width and the height of the met domain in meters? Extents will be generated based on the
 # location of chosen point.
-width_met_domain_m <- 30000
-height_met_domain_m <- 30000
+met_domain_width_m <- 30000
+met_domain_height_m <- 30000
 
 # Do these length and width values accomodate an integer number of cells of the specified resolution?
 # These checks will be later part of a function in setting domain width and height
@@ -71,7 +71,78 @@ number_cells_across_y <- ifelse(is_number_cells_across_y_an_int == TRUE,
 
 total_cells <- number_cells_across_x * number_cells_across_y
 
+# Get extents of grid
+
+# Westing is [1,1], Northing is [1,2]
+UTM_location[1,1]
+
+# Choices are: 1 (center), 2 (lower left), 3 (lower right), 4 (upper left), 5 (upper right)
+
+
+left_UTM <- if(lat_long_grid_loc == 1) {
+          UTM_location[1,1] - (0.5 * met_domain_width_m)
+          } else if (lat_long_grid_loc == 2) {
+          UTM_location[1,1]
+          } else if (lat_long_grid_loc == 3) {
+          UTM_location[1,1] - met_domain_width_m
+          } else if (lat_long_grid_loc == 4) {
+            UTM_location[1,1]
+          } else if (lat_long_grid_loc == 5) {
+            UTM_location[1,1] - met_domain_width_m
+          } else {
+          NULL
+          }
+
+right_UTM <- if(lat_long_grid_loc == 1) {
+          UTM_location[1,1] + (0.5 * met_domain_width_m)
+          } else if (lat_long_grid_loc == 2) {
+          UTM_location[1,1] + met_domain_width_m
+          } else if (lat_long_grid_loc == 3) {
+          UTM_location[1,1]
+          } else if (lat_long_grid_loc == 4) {
+          UTM_location[1,1] + met_domain_width_m
+          } else if (lat_long_grid_loc == 5) {
+          UTM_location[1,1]
+          } else {
+          NULL
+          }
+
+bottom_UTM <- if(lat_long_grid_loc == 1) {
+          UTM_location[1,2] - (0.5 * met_domain_height_m)
+          } else if (lat_long_grid_loc == 2) {
+          UTM_location[1,2]
+          } else if (lat_long_grid_loc == 3) {
+          UTM_location[1,2]
+          } else if (lat_long_grid_loc == 4) {
+          UTM_location[1,2] - met_domain_height_m
+          } else if (lat_long_grid_loc == 5) {
+          UTM_location[1,2] - met_domain_height_m
+          } else {
+          NULL
+          }
+
+top_UTM <- if(lat_long_grid_loc == 1) {
+        UTM_location[1,2] + (0.5 * met_domain_height_m)
+        } else if (lat_long_grid_loc == 2) {
+        UTM_location[1,2] + met_domain_height_m
+        } else if (lat_long_grid_loc == 3) {
+        UTM_location[1,2] + met_domain_height_m
+        } else if (lat_long_grid_loc == 4) {
+        UTM_location[1,2]
+        } else if (lat_long_grid_loc == 5) {
+        UTM_location[1,2]
+        } else {
+        NULL
+        }
+
+
+##----- SURF.DAT ---- Surface data
+
 # Get surface data from NCDC, download via FTP
+
+# 
+#
+
 
 # Determine which surface stations are in the domain
 
