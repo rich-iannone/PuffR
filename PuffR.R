c25ed4a8adde15cc093bd9c82cadaaea3166b022
rich-iannone <riannone@me.com>
2013-10-11 12:59:55 -0700

    Included code to download hourly surface data from NOAA/NCDC.

--- PuffR.R
+++ PuffR.R
@@ -1,11 +1,11 @@
-##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##
-##                                                                  ##
-## R project to generate useful tool for dispersion modelling with  ##
-## the CALPUFF system                                               ##
-##                                                                  ##
-## Rich Iannone, 2013                                               ##
-##                                                                  ##
-##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##
+##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##
+##                                                                      ##
+##  R project to generate useful tools for dispersion modelling with    ##
+##  the CALPUFF system                                                  ##
+##                                                                      ##
+##  Rich Iannone, 2013                                                  ##
+##                                                                      ##
+##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##
 
 # Load in necessary packages
 install.packages("sp")
@@ -21,9 +21,8 @@ library("fields")
 install.packages("MBA")
 library("MBA")
 
+
 # Start with information on location for CALMET domain
-# Center of domain? LL? UR?
-# Coordinate Systems?
 
 # Let's start with lat/long in decimal degrees, convert to UTM, then develop a grid
 
@@ -77,14 +76,7 @@ number_cells_across_y <- ifelse(is_number_cells_across_y_an_int == TRUE,
 
 total_cells <- number_cells_across_x * number_cells_across_y
 
-# Get extents of grid
-
-# Westing is [1,1], Northing is [1,2]
-UTM_location[1,1]
-
-# Choices are: 1 (center), 2 (lower left), 3 (lower right), 4 (upper left), 5 (upper right)
-
-
+# Get extents of UTM grid (left, right, bottom, top)
 left_UTM <- if(lat_long_grid_loc == 1) {
           UTM_location[1,1] - (0.5 * met_domain_width_m)
           } else if (lat_long_grid_loc == 2) {
@@ -144,13 +136,53 @@ top_UTM <- if(lat_long_grid_loc == 1) {
 
 ##----- SURF.DAT ---- Surface data
 
-# Get surface data from NCDC, download via FTP
+# Get hourly surface data history CSV from NOAA/NCDC FTP
+file <- "ftp://ftp.ncdc.noaa.gov/pub/data/noaa/ish-history.csv"
+repeat {
+  try(download.file(file, "ish-history.csv",
+                    quiet = TRUE))
+  if (file.info("ish-history.csv")$size >
+        0) {
+    break
+  }
+}
 
+# Read in the CSV file
+st <- read.csv("ish-history.csv")
 
+# Get formatted list of stations from Canada ("CN")
+names(st)[c(3, 10)] <- c("NAME", "ELEV")
+st <- st[, -5]
+st <- st[st$CTRY == "CN", ]
 
+# Reintroduce the decimal
+st$LAT <- st$LAT/1000
+st$LON <- st$LON/1000
+st$ELEV <- st$ELEV/10
 
-# Determine which surface stations are in the domain
+# Recompose the years from the data file
+st$BEGIN <- as.numeric(substr(st$BEGIN, 1, 4))
+st$END <- as.numeric(substr(st$END, 1, 4))
 
+# Generate a list of stations for BC based on beginning and end years, ignoring stations without
+# beginning years reported.
+bc.list <- st[st$STATE == "BC" & (st$BEGIN <= 2010 &
+                                    st$END >= 2012 & !is.na(st$BEGIN)), ]
 
+# Initialize data frame for file status reporting
+outputs <- as.data.frame(matrix(NA, dim(bc.list)[1], 2))
+names(outputs) <- c("FILE", "STATUS")
 
-# Construct FTP address
+# Download the gzip-compressed data files for 2010 (in this case)
+for (y in 2010:2010) {
+  y.bc.list <- bc.list[bc.list$BEGIN <= y & bc.list$END >= y, ]
+  for (s in 1:dim(y.bc.list)[1]) {
+    outputs[s, 1] <- paste(sprintf("%06d", y.bc.list[s,1]),
+                           "-", sprintf("%05d", y.bc.list[s,2]),
+                           "-", y, ".gz", sep = "")
+    system(paste("curl -O ftp://ftp3.ncdc.noaa.gov/pub/data/noaa/", y,
+                     "/", outputs[s, 1], sep = ""))
+  }
+}
+
+system("gunzip -r data/raw", intern = FALSE, ignore.stderr = TRUE)
