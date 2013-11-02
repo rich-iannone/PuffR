af4a8ae37b3e1635832313cc10942635ec845498
rich-iannone <riannone@me.com>
2013-11-02 01:52:38 -0700

    Correct the syntax in use of 'exists' function
    Wrapped the first (and only) argument in quotes.

--- PuffR.R
+++ PuffR.R
@@ -194,10 +194,10 @@ use.manual.bounds <- FALSE
   } else { }
 
 # Check whether the CALMET domain bounds (from the function 'define.calmet.domain') are available
-  if (!exists(latlong_bbox_west) |
-      !exists(latlong_bbox_east) |  
-      !exists(latlong_bbox_north) |
-      !exists(latlong_bbox_south)) {
+  if (!exists("latlong_bbox_west") |
+      !exists("latlong_bbox_east") |  
+      !exists("latlong_bbox_north") |
+      !exists("latlong_bbox_south")) {
     stop("The CALMET domain hasn't been defined. Please define a domain enter bounds manually.")
   } else { }
 
