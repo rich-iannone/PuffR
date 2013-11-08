2cbdaf89b28f2d03d3a0d2c224bea2f4fbd8d583
rich-iannone <riannone@me.com>
2013-11-08 01:33:44 -0800

    Change logic in downloading NOAA/NCDC history file
    Revised code to better determine whether the history
    file already exists in the working directory.

--- PuffR.R
+++ PuffR.R
@@ -190,20 +190,25 @@ print(define.calmet.domain.out)
 ## Function start #### get.ncdc.history ###########################################
 get.ncdc.history <- function(replace.file = FALSE) {
 
-# Check if file exists in working directory
-  if (file.exists("ish-history.csv") &
-      file.info("ish-history.csv")$size > 0 &
-      replace.file == FALSE) {
-  stop("The 'ish-history.csv' file is available in the working directory.
-       To download again, use 'replace.file = TRUE'")
-  } else { }
-  
 # Get hourly surface data history CSV from NOAA/NCDC FTP
 file <- "ftp://ftp.ncdc.noaa.gov/pub/data/noaa/ish-history.csv"
-repeat {
-  try(download.file(file, "ish-history.csv", quiet = TRUE))
-  if (file.info("ish-history.csv")$size > 0) { break }
+
+if (replace.file == TRUE) {
+  repeat {
+    try(download.file(file, "ish-history.csv", quiet = TRUE))
+    if (file.info("ish-history.csv")$size > 0) { break }
   }
+} else { }
+
+# Check if file exists in working directory
+if (file.exists("ish-history.csv") &
+      file.info("ish-history.csv")$size > 0 &
+      replace.file == FALSE) { } else { 
+        repeat {
+          try(download.file(file, "ish-history.csv", quiet = TRUE))
+          if (file.info("ish-history.csv")$size > 0) { break }
+        }
+      }
 
 # Read in the "ish-history" CSV file
 st <- read.csv("ish-history.csv")
