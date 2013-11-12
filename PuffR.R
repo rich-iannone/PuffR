fad764c0c0b99aeb72f7ef654883ee28370ed893
rich-iannone <riannone@me.com>
2013-11-11 16:46:39 -0800

    Change temperature cutoff for missing value

--- PuffR.R
+++ PuffR.R
@@ -366,7 +366,7 @@ for (i in 1:length(files)) {
     data$LONG <- data$LONG/1000
     data$WIND.DIR <- ifelse(data$WIND.DIR == 999, 999, data$WIND.DIR)
     data$WIND.SPD <- ifelse(data$WIND.SPD > 100, 999.9, data$WIND.SPD/10)
-    data$TEMP <-  ifelse(data$TEMP > 100, 999.9, round((data$TEMP/10) + 273.2, 1))
+    data$TEMP <-  ifelse(data$TEMP > 900, 999.9, round((data$TEMP/10) + 273.2, 1))
     data$DEW.POINT <- ifelse(data$DEW.POINT > 100, 999.9, data$DEW.POINT/10)
     data$ATM.PRES <- ifelse(data$ATM.PRES > 2000, 999.9, data$ATM.PRES/10)
     data$CEIL.HGT <- ifelse(data$CEIL.HGT == 99999, 999.9, round(data$CEIL.HGT*3.28084/100, 0))
