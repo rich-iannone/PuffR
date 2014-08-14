658558c4dd9d5e3c4cffe61a6287cfd1902f9a11
rich-iannone <riannone@me.com>
2014-08-14 16:53:49 -0700

    Add function arguments and default values

--- R/grid.define.proj.R
+++ R/grid.define.proj.R
@@ -1,3 +1,11 @@
+2_define_proj_levels <- function(pmap = "UTM",
+                                 feast = 0.0, fnorth = 0.0,
+                                 iutmzn, utmhem = "N",
+                                 rlat0, rlon0,
+                                 xlat1, xlat2,
+                                 datum, nx, ny,
+                                 dgridkm, xorigkm, yorigkm,
+                                 nz, zface){
     
     #
     # Define grid projection and datum
