4d28e9246abd2480721c5b13a719b137a8f5ee9f
rich-iannone <riannone@me.com>
2014-08-16 23:59:54 -0700

    Add script for 'calmet_4_met_data_opts' function

---
+++ R/calmet_4_met_data_opts.R
@@ -0,0 +1,9 @@
+#' Set CALMET parameters for the meteorological data options
+#' @description This function validates and writes CALMET parameters for the meteorological data options to the working CALMET.INP file.
+#' @param calmet_inp the absolute path and filename for the working CALMET input file.
+#' @export calmet_4_met_data_opts
+
+calmet_4_met_data_opts <- function(calmet_inp){
+  
+  
+}
