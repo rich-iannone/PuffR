b099827b150cb19dcafb80846f6a10db3e1f238f
rich-iannone <riannone@me.com>
2014-08-18 07:33:14 -0700

    Add multiple @param statements in Roxygen section

--- R/calmet_4_met_data_opts.R
+++ R/calmet_4_met_data_opts.R
@@ -1,6 +1,13 @@
 #' Set CALMET parameters for the meteorological data options
 #' @description This function validates and writes CALMET parameters for the meteorological data options to the working CALMET.INP file.
 #' @param calmet_inp the absolute path and filename for the working CALMET input file.
+#' @param noobs option for 'no observation mode' where 0 represents the use of surface, overwater, and upper air stations; 1 represents the use of surface and overwater stations (no upper air observations), and MM4/MM5/3D data files for upper air information; and 3 represents having no surface, overwater, or upper air observations used (use instead MM4/MM5/3D data files for surface, overwater, and upper air data).
+#' @param nssta number of surface meteorological stations used.
+#' @param npsta number of precipitation stations used; a value of -1 is to be provided when MM5/3D precip data is used.
+#' @param icloud represents the cloud data option to use, where 0 indicates that gridded cloud information is not to be used; 1 represents the option for gridded cloud.dat file to be generated as output; the use of option 2 states that the gridded cloud.dat file will be read in as input; and option 3 is for computing gridded cloud cover from prognostic fields.
+#' @param iforms represents two possible options for the surface meteorological data file format: 1 - unformatted (e.g., SMERGE output); 2 - formatted (as free-formatted user input).
+#' @param iformp represents two possible options for the precipitation data file format: 1 - unformatted (e.g., PMERGE output); 2 - formatted (free-formatted user input).
+#' @param iformc represents two possible options for the cloud data file format: 1 - unformatted as CALMET unformatted output; 2 - formatted (free-formatted CALMET output or user input).
 #' @export calmet_4_met_data_opts
 
 calmet_4_met_data_opts <- function(calmet_inp,
