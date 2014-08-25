889f79b4613a3ed0b858af6d6a9b57055f8dc20b
rich-iannone <riannone@me.com>
2014-08-25 08:50:46 -0700

    Add text for all @param values

--- R/calmet_6_mixhgt_temp_precip_params.R
+++ R/calmet_6_mixhgt_temp_precip_params.R
@@ -1,42 +1,42 @@
 #' Set CALMET parameters for mixing height, temperature, and precipitation
 #' @description This function validates and writes CALMET parameters for wind field options and parameters to the working CALMET.INP file.
 #' @param calmet_inp the absolute path and filename for the working CALMET input file.
-#' @param constb 
-#' @param conste 
-#' @param constn 
-#' @param constw 
-#' @param fcoriol 
-#' @param iavezi 
-#' @param mnmdav 
-#' @param hafang 
-#' @param ilevzi 
-#' @param imixh 
-#' @param threshl 
-#' @param threshw 
-#' @param itwprog 
-#' @param iluoc3d 
-#' @param dptmin 
-#' @param dzzi 
-#' @param zimin 
-#' @param zimax 
-#' @param ziminw 
-#' @param zimaxw 
-#' @param icoare 
-#' @param dshelf 
-#' @param iwarm 
-#' @param icool 
-#' @param itprog 
-#' @param irad 
-#' @param tradkm 
-#' @param numts 
-#' @param iavet 
-#' @param tgdefb 
-#' @param tgdefa 
-#' @param jwat1 
-#' @param jwat2 
-#' @param nflagp 
-#' @param sigmap 
-#' @param cutp 
+#' @param constb empirical mixing height constant for the neutral, mechanical equation.
+#' @param conste empirical mixing height constant for the convective mixing height equation.
+#' @param constn empirical mixing height constant for the stable mixing height equation.
+#' @param constw empirical mixing height constant for the overwater mixing height equation.
+#' @param fcoriol absolute value of the Coriolis parameter.
+#' @param iavezi conduct spatial averaging of mixing heights?
+#' @param mnmdav maximum search radius (in grid cell units) for averaging of mixing heights.
+#' @param hafang half-angle of upwind looking cone for averaging of mixing heights.
+#' @param ilevzi layer of winds used in upwind averaging of mixing heights.
+#' @param imixh method for computation of the convective mixing height: (1) Maul-Carson for land and water cells, (-1) Maul-Carson for land and water cells and OCD mixing heights overwater, (2) Batchvarova and Gryning for land and water cells, and (-2) Batchvarova and Gryning for land and water cells and OCD mixing heights overwater. 
+#' @param threshl threshold buoyancy flux required to sustain convective mixing height growth overland. Expressed as a heat flux per meter of boundary layer in units of W/m3.
+#' @param threshw threshold buoyancy flux required to sustain convective mixing height growth overwater. Expressed as a heat flux per meter of boundary layer in units of W/m3.
+#' @param itwprog option for overwater lapse rates used in convective mixing height growth: (0) use SEA.DAT lapse rates and deltaT (or assume neutral conditions if missing), (1) use prognostic lapse rates (only if IPROG > 2) and SEA.DAT deltaT (or neutral if missing), (2) use prognostic lapse rates and prognostic delta T (only if IPROG > 12 and 3D.DAT version 2.0 or higher).
+#' @param iluoc3d land use category for ocean in 3D.DAT datasets.
+#' @param dptmin minimum potential temperature lapse rate in the stable layer above the current convective mixing height (in units of K/m).
+#' @param dzzi depth of layer (in meters) above current convective mixing height through which lapse rate is computed.
+#' @param zimin minimum overland mixing height (in meters).
+#' @param zimax maximum overland mixing height (in meters).
+#' @param ziminw minimum overwater mixing height (in meters).
+#' @param zimaxw maximum overwater mixing height (in meters).
+#' @param icoare method for computation of overwater surface fluxes: (0) original deltaT method, (10) COARE with no wave parameterization (jwave = 0), (11) COARE with wave option (jwave = 1) and default wave properties, (-11) COARE with wave option (jwave = 1) and observed wave properties (must be in SEA.DAT files), (12) COARE with wave option 2 (Taylor and Yelland) and default wave properties, and (-12) COARE with wave option 2 (Taylor and Yelland) and observed wave properties (must be in SEA.DAT files). 
+#' @param dshelf coastal/shallow water length scale (in kilometers) for modified z0 in shallow water.
+#' @param iwarm COARE warm layer computation: (0) off, (1) on.
+#' @param icool COARE cool skin layer computation: (0) off, (1) on.
+#' @param itprog selection of temperature data from observations or prognostic data: (0) use both surface and upper air stations, (1) use both surface stations and MM5/3D files for upper air data (no upper air observations), (2) MM5/3D files for both the surface and upper air data (no surface or upper air observations).
+#' @param irad interpolation type: (1) 1/R, (2) 1/R^2.
+#' @param tradkm radius of influence (in kilometers) for temperature interpolation.
+#' @param numts maximum number of stations to include in temperature interpolation.
+#' @param iavet conduct spatial averaging of temperatures?
+#' @param tgdefb default temperature gradient (in units of K/m) below the mixing height over water.
+#' @param tgdefa default temperature gradient (in units of K/m) above the mixing height over water.
+#' @param jwat1 beginning land use category value for temperature interpolation over water.
+#' @param jwat2 ending land use category value for temperature interpolation over water.
+#' @param nflagp method of interpolation for precipitation: (1) 1/R, (2) 1/R^2, (3) exp/R^2.
+#' @param sigmap radius of influence (in kilometers) for interpolation of precipitation.
+#' @param cutp minimum precipitation rate cutoff (in units of mm/h).
 #' @export calmet_6_mixhgt_temp_precip_params
 
 calmet_6_mixhgt_temp_precip_params <- function(calmet_inp,
