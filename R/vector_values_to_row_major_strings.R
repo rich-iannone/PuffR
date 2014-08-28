01d6adf5d57de93459e731f5d22b630d2fde3af8
rich-iannone <riannone@me.com>
2014-08-28 13:05:59 -0700

    Remove extra whitespace from script

--- R/vector_values_to_row_major_strings.R
+++ R/vector_values_to_row_major_strings.R
@@ -15,13 +15,9 @@ vector_values_to_row_major_strings <- function(values_vector,
   # Generate a vector of comma-delimited strings containing LU categories of every row of cells;
   # this is for writing to a file and eventual inclusion in the GEO.DAT file
   for (i in 1:nrow(values_df)){
-    
     if (i == 1) values_strings <- vector(mode = "character", length = 0)
-    
     string <- paste(values_df[i, ], collapse = ", ")
-    
     values_strings <- c(values_strings, string)
-    
   }
   
   return(values_strings)
