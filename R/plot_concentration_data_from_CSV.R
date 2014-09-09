661ecdf2951ae5604288cb0f724471a5627c27aa
rich-iannone <riannone@me.com>
2014-09-09 16:50:42 -0700

    Obtain a file name for each PDF that was to be generated

--- R/plot_concentration_data_from_CSV.R
+++ R/plot_concentration_data_from_CSV.R
@@ -116,4 +116,7 @@ plot_concentration_data_from_CSV <- function(CSV_file_pattern,
     for (i in 1:length(file_list)){
       if (i == 1) PDF_list <- vector(mode = "character", length = 0)
       
+      # Obtain a file name for each PDF that was to be generated
+      a_PDF <- paste(gsub(".csv", "", file_list[i]), ".pdf", sep = '')
+      
 }
