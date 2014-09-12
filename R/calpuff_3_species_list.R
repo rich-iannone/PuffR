e73d8ccedd4056dc4fe29fd14150d8f2e190f533
rich-iannone <riannone@me.com>
2014-09-11 21:12:45 -0700

    Add script for generating segment of CALPUFF input file

---
+++ R/calpuff_3_species_list.R
@@ -0,0 +1,19 @@
+#' Set the CALPUFF species list
+#' @description This function validates and writes CALPUFF parameters for the model's species list.
+#' @param calpuff_inp 
+#' @param species_names_cspec 
+#' @param species_modelled 
+#' @param species_emitted 
+#' @param species_ddep 
+#' @param species_group_number 
+#' @export calpuff_3_species_list
+
+calpuff_3_species_list <- function(calpuff_inp,
+                                   species_names_cspec = NULL,
+                                   species_modelled = NULL,
+                                   species_emitted = NULL,
+                                   species_ddep = NULL,
+                                   species_group_number = NULL){
+    
+}
+                                     
