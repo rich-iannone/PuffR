956da077c78bc680e1a98c45700e979cbb673b55
rich-iannone <riannone@me.com>
2014-09-11 21:12:25 -0700

    Add script for generating segment of CALPUFF input file

---
+++ R/calpuff_10_wdep_params.R
@@ -0,0 +1,12 @@
+#' Set the CALPUFF parameters for wet deposition
+#' @description This function validates and writes CALPUFF parameters for wet deposition.
+#' @param calpuff_inp 
+#' @param species_scaveng_coeff_liquid_precip 
+#' @param species_scaveng_coeff_froze_precip 
+#' @export calpuff_10_wdep_params
+
+calpuff_10_wdep_params <- function(calpuff_inp,
+                                   species_scaveng_coeff_liquid_precip = NULL,
+                                   species_scaveng_coeff_froze_precip = NULL){
+  
+}
