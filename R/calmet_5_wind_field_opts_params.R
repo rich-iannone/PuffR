8bcde833291193ef8ccf5b620c37468b2cf93a2c
rich-iannone <riannone@me.com>
2014-09-11 21:10:40 -0700

    Write output to the same working calmet.inp file

--- R/calmet_5_wind_field_opts_params.R
+++ R/calmet_5_wind_field_opts_params.R
@@ -139,5 +139,7 @@ calmet_5_wind_field_opts_params <- function(calmet_inp,
                                        keyword = keywords,
                                        replacement = replacements)    
   
+  # Write the output to the same working calmet.inp file
+  writeLines(calmet_inp_working, con = calmet_inp)
   
 }
