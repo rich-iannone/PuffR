f1064c3fc163d950f132d1c511b2b9087d689a86
rich-iannone <riannone@me.com>
2014-09-11 21:13:08 -0700

    Add script for generating segment of CALPUFF input file

---
+++ R/calpuff_8_size_ddep_particles.R
@@ -0,0 +1,12 @@
+#' Set the CALPUFF size parameters for dry deposition of particles
+#' @description This function validates and writes CALPUFF size parameters for dry deposition of particles.
+#' @param calpuff_inp 
+#' @param species_geom_mass_mean_diam 
+#' @param species_geom_SD 
+#' @export calpuff_8_size_ddep_particles
+
+calpuff_8_size_ddep_particles <- function(calpuff_inp,
+                                          species_geom_mass_mean_diam = NULL,
+                                          species_geom_SD = NULL){
+  
+}
