cf0851db737a9c6904a9dc2ab5521d70a097720e
rich-iannone <riannone@me.com>
2014-09-11 21:10:32 -0700

    Write output to the same working calmet.inp file

--- R/calmet_3_output_opts.R
+++ R/calmet_3_output_opts.R
@@ -82,5 +82,8 @@ calmet_3_output_opts <- function(calmet_inp,
                                        keyword = keywords,
                                        replacement = replacements)    
   
+  # Write the output to the same working calmet.inp file
+  writeLines(calmet_inp_working, con = calmet_inp)
+  
   
 }
