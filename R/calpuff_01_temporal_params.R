fcb1a216d095df10a08961d534df2f60717a0582
rich-iannone <riannone@me.com>
2014-09-15 01:36:39 -0700

    Correct several typos in comments

--- R/calpuff_01_temporal_params.R
+++ R/calpuff_01_temporal_params.R
@@ -37,7 +37,7 @@ calpuff_01_temporal_params <- function(calpuff_inp,
                                       itest,
                                       mreg){
   
-  # Generate a vector list of calmet.inp keywords
+  # Generate a vector list of calpuff.inp keywords
   keywords <- c("IBYR", "IBMO", "IBDY", "IBHR", "IBSEC",
                 "IEYR", "IEMO", "IEDY", "IEHR", "IESEC",
                 "ABTZ", "NSECDT", "IRTYPE", "LCALGRD", "ITEST", 
@@ -49,12 +49,12 @@ calpuff_01_temporal_params <- function(calpuff_inp,
                     abtz, nsecdt, irtype, lcalgrd, itest,
                     mreg)
   
-  # Modify all parameters in working calmet.inp vector
+  # Modify all parameters in working calpuff.inp vector
   calpuff_inp_working <- replace_in_inp(inp_file_working = calpuff_inp_working,
                                         keyword = keywords,
                                         replacement = replacements)
   
-  # Write the output to the same working calmet.inp file
+  # Write the output to the same working calpuff.inp file
   writeLines(calpuff_inp_working, con = calpuff_inp)
   
 }
