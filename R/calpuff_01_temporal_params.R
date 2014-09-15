c67dc9eee3038f678846ccbf48a9414b91d668ba
rich-iannone <riannone@me.com>
2014-09-15 01:39:54 -0700

    Correct several typos throughout script

--- R/calpuff_01_temporal_params.R
+++ R/calpuff_01_temporal_params.R
@@ -1,6 +1,6 @@
-#' Set CALPUFF temporal and model run parameters
-#' @description This function validates and writes CALPUFF parameters for the model starting and ending times, the time zone, and model timings.
-#' @param calpuff_inp 
+#' Set CALMET temporal and model run parameters
+#' @description This function validates and writes CALMET parameters for the model starting and ending times, the time zone, and model timings.
+#' @param calmet_inp 
 #' @param ibyr 
 #' @param ibmo 
 #' @param ibdy 
@@ -17,9 +17,9 @@
 #' @param lcalgrd 
 #' @param itest 
 #' @param mreg 
-#' @export calpuff_01_temporal_params
+#' @export calmet_01_temporal_params
 
-calpuff_01_temporal_params <- function(calpuff_inp,
+calmet_01_temporal_params <- function(calmet_inp,
                                       ibyr,
                                       ibmo,
                                       ibdy,
@@ -37,7 +37,9 @@ calpuff_01_temporal_params <- function(calpuff_inp,
                                       itest,
                                       mreg){
   
-  # Generate a vector list of calpuff.inp keywords
+  
+  
+  # Generate a vector list of calmet.inp keywords
   keywords <- c("IBYR", "IBMO", "IBDY", "IBHR", "IBSEC",
                 "IEYR", "IEMO", "IEDY", "IEHR", "IESEC",
                 "ABTZ", "NSECDT", "IRTYPE", "LCALGRD", "ITEST", 
@@ -49,12 +51,12 @@ calpuff_01_temporal_params <- function(calpuff_inp,
                     abtz, nsecdt, irtype, lcalgrd, itest,
                     mreg)
   
-  # Modify all parameters in working calpuff.inp vector
-  calpuff_inp_working <- replace_in_inp(inp_file_working = calpuff_inp_working,
-                                        keyword = keywords,
-                                        replacement = replacements)
+  # Modify all parameters in working calmet.inp vector
+  calmet_inp_working <- replace_in_inp(inp_file_working = calmet_inp_working,
+                                       keyword = keywords,
+                                       replacement = replacements)
   
-  # Write the output to the same working calpuff.inp file
-  writeLines(calpuff_inp_working, con = calpuff_inp)
+  # Write the output to the same working calmet.inp file
+  writeLines(calmet_inp_working, con = calmet_inp)
   
 }
