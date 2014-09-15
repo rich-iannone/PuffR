e8f62b9f1afda59110e075a90953b54048f29f41
rich-iannone <riannone@me.com>
2014-09-15 01:28:54 -0700

    Remove temporary statements from file

--- R/replace_in_inp.R
+++ R/replace_in_inp.R
@@ -14,9 +14,6 @@ replace_in_inp <- function(inp_file_working,
     
     inp_file_working[grep(keyword[i], inp_file_working)] <-
     gsub("=.*!", paste("= ", replacement[i], " !", sep = ''),
-         inp_file_working[grep(paste(keyword[i], "[ ]*=", sep = ''), inp_file_working)])
-
-    gsub("=.*!", paste("= ", replacement[i], " !", sep = ''),
          inp_file_working[grep(paste(keyword[i], "(?![[:alpha:]])", sep = ''),
                                inp_file_working, perl = TRUE)])
   
