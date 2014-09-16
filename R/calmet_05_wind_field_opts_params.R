287e9786ff9f8ed7fa8fa745f2fd4a24540fb6b8
rich-iannone <riannone@me.com>
2014-09-15 17:51:31 -0700

    Remove several objects from vector list

--- R/calmet_05_wind_field_opts_params.R
+++ R/calmet_05_wind_field_opts_params.R
@@ -132,11 +132,12 @@ calmet_05_wind_field_opts_params <- function(calmet_inp = "calmet_template.txt",
   # Generate a formatted character string for 'zupwnd'
   zupwnd <- paste(zupwnd, collapse = ", ")
   
+  # Generate a vector list of calmet.inp keywords that require single values
   keywords <- c("IWFCOD", "IFRADJ", "IKINE", "IOBR", "ISLOPE", "IEXTRP", "ICALM",
-                "BIAS", "RMIN2", "IPROG", "ISTEPPG", "IGFMET", "LVARY", "RMAX1", "RMAX2", "RMAX3",
-                "RMIN", "TERRAD", "R1", "R2", "RPROG", "DIVLIM", "NITER", "NSMTH", "NINTR2", "CRITFN",
-                "ALPHA", "FEXTR2", "NBAR", "KBAR", "XBBAR", "YBBAR", "XEBAR", "YEBAR", "IDIOPT1",
-                "ISURFT", "IDIOPT2", "IUPT", "ZUPT", "IDIOPT3", "IUPWND", "ZUPWND", "IDIOPT4", "IDIOPT5",
+                "RMIN2", "IPROG", "ISTEPPG", "IGFMET", "LVARY", "RMAX1", "RMAX2", "RMAX3",
+                "RMIN", "TERRAD", "R1", "R2", "RPROG", "DIVLIM", "NITER", "NINTR2", "CRITFN",
+                "ALPHA", "NBAR", "KBAR", "XBBAR", "YBBAR", "XEBAR", "YEBAR", "IDIOPT1",
+                "ISURFT", "IDIOPT2", "IUPT", "ZUPT", "IDIOPT3", "IUPWND", "IDIOPT4", "IDIOPT5",
                 "LLBREZE", "NBOX", "XG1", "XG2", "YG1", "YG2", "XBCST", "YBCST", "XECST", "YECST",
                 "NLB", "METBXID")
   
