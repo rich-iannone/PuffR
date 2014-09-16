65e51acc751e5c8ce66371b8350b145b7ee42318
rich-iannone <riannone@me.com>
2014-09-15 17:43:39 -0700

    Add function 'calmet_00_filenames' to package

---
+++ R/calmet_00_filenames.R
@@ -0,0 +1,41 @@
+#' Set input and output filenames for CALMET model run
+#' @description This function sets the input and output filenames for CALMET model runs in the working CALMET.INP file.
+#' @param calmet_inp the absolute path and filename for the working CALMET input file.
+#' @export calmet_00_filenames
+
+calmet_00_filenames <- function(calmet_inp = "calmet_template.txt",
+                                geodat = NULL,
+                                srfdat = NULL,
+                                clddat = NULL,
+                                prcdat = NULL,
+                                wtdat = NULL,
+                                metlst = NULL,
+                                metdat = NULL,
+                                pacdat = NULL,
+                                lcfiles = TRUE,
+                                nusta = NULL,
+                                nowsta = NULL,
+                                nm3d = NULL,
+                                nigf = NULL,
+                                updat = NULL,
+                                seadat = NULL,
+                                m3ddat = NULL,
+                                igfdat = NULL,
+                                diadat = NULL,
+                                prgdat = NULL,
+                                tstprt = NULL,
+                                tstout = NULL,
+                                tstkin = NULL,
+                                tstfrd = NULL,
+                                tstslp = NULL,
+                                dcstgd = NULL){
+    
+  
+  # Read in the working calmet.inp file as a character vector
+  calmet_inp_working <- readLines(calmet_inp)
+    
+  # Write the output to the same working calmet.inp file
+  writeLines(calmet_inp_working, con = calmet_inp)
+  
+}
+
