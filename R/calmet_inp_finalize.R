9823b9b3c63fa7c352ab796e6b1492125d7f23a8
rich-iannone <riannone@me.com>
2014-09-17 01:55:22 -0700

    Make replacement to ending year

--- R/calmet_inp_finalize.R
+++ R/calmet_inp_finalize.R
@@ -370,6 +370,11 @@ calmet_inp_finalize <- function(calmet_inp = "calmet_template.txt",
                                            keyword = "IBHR",
                                            replacement = 0)
       
+      # Make replacement to ending year
+      calmet_inp_working <- replace_in_inp(inp_file_working = calmet_inp_working,
+                                           keyword = "IEYR",
+                                           replacement = begin_end_year)
+      
       
       # Write the modified 'calmet_inp_working' vector object as
       # a CALMET input file
