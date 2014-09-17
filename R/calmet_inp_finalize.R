cf9e8790737b2f9aa86b55568ef73cfa58535c63
rich-iannone <riannone@me.com>
2014-09-17 01:43:38 -0700

    Determine ending month for input file

--- R/calmet_inp_finalize.R
+++ R/calmet_inp_finalize.R
@@ -336,6 +336,11 @@ calmet_inp_finalize <- function(calmet_inp = "calmet_template.txt",
                                    "\\1",
                                    geo_header_dates))
       
+      # Determine ending month for input file
+      end_month <- as.numeric(gsub(".*\\([0-9][0-9] [0-9][0-9] - ([0-9][0-9]).*",
+                                   "\\1",
+                                   geo_header_dates))
+      
       # Write the modified 'calmet_inp_working' vector object as
       # a CALMET input file
       writeLines(calmet_inp_working,
