ecafc2949ac245064441c669821ba7329ab3df3c
rich-iannone <riannone@me.com>
2014-09-27 00:41:52 -0700

    Add function 'calpuff_add_point_sources'

---
+++ R/calpuff_add_point_sources.R
@@ -0,0 +1,7 @@
+#' Add point sources to a list for later use in CALPUFF
+#' @description Add point sources to a list for later use in CALPUFF
+#' @export calpuff_add_point_sources
+
+calpuff_add_point_sources <- function(){
+  
+}
