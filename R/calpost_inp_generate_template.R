1343df7ec92db7c3fbcf1d0a36ad0222ae1762ff
rich-iannone <riannone@me.com>
2014-09-27 21:22:25 -0700

    Add function 'calpost_inp_generate_template'

---
+++ R/calpost_inp_generate_template.R
@@ -0,0 +1,7 @@
+#' Generate a template for a CALPOST input file
+#' @description Generate a template for a CALPOST input file.
+#' @export calpost_inp_generate_template
+
+calpost_inp_generate_template <- function(){
+  
+}
