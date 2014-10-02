e861cf019bde5641189dccc5f100e309e4deaec0
rich-iannone <riannone@me.com>
2014-10-01 23:29:33 -0700

    Add function 'calmet_exec' to package

---
+++ R/calmet_exec.R
@@ -0,0 +1,8 @@
+#' Run the CALMET model
+#' @description Run the CALMET model.
+#' @param calmet_exec the path and name of the CALMET executable.
+#' @export calmet_exec
+
+calmet_exec <- function(calmet_exec){
+  
+}
