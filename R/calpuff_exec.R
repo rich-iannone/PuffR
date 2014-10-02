0ee0658b7816822a3e3f780c65a46119e0fd746b
rich-iannone <riannone@me.com>
2014-10-02 16:54:46 -0700

    Add function 'calpuff_exec' to package

---
+++ R/calpuff_exec.R
@@ -0,0 +1,26 @@
+#' Run the CALPUFF model
+#' @description Run the CALPUFF model.
+#' @param calpuff_exec the path and name of the CALPUFF executable.
+#' @export calpuff_exec
+
+calpuff_exec <- function(calpuff_exec){
+  
+  # Obtain list of CALPUFF input files
+  calpuff_in_files <- list.files(pattern = "^calpuff_in.*.txt")
+  
+  # Process each input file with CALPUFF
+  for (i in 1:length(calpuff_in_files)){
+    
+    # Run the input file with CALPUFF and capture output as lines to 'console_log'
+    console_log <- system(command = paste("cd ", getwd(), " ; '",
+                                          calpuff_exec, "' ", calpuff_in_files[i],
+                                          sep = ""),
+                          intern = TRUE)
+    
+    # Write 'console_log' to a text file
+    writeLines(console_log, con = paste("log_for_", calpuff_in_files[i], sep = '' ))
+    
+  }
+  
+}
+
