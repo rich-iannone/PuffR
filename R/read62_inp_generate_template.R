844f73fb3a3828d6c6ab9328df0b0a7931e7b7a5
rich-iannone <riannone@me.com>
2014-10-02 17:12:46 -0700

    Add function 'read62_inp_generate_template'

---
+++ R/read62_inp_generate_template.R
@@ -0,0 +1,54 @@
+#' Generate a template for a READ62 input file
+#' @description Generate a template for a READ62 input file.
+#' @export read62_inp_generate_template
+
+read62_inp_generate_template <- function(){
+  
+  writeLines(
+    c("READ62.INP      2.1             Hour Start and End Times with Seconds                           ",
+      "",
+      "0 -- Input and Output Files",
+      "",
+      "! INDAT  = !",
+      "! SUBDAT = !",
+      "! UPDAT  = !",
+      "! RUNLST = !",
+      "",
+      "! LCFILES = T !",
+      "",
+      "!END!",
+      "",
+      "--------------------------------------------------------------------------------",
+      "",
+      "1 -- Run control parameters",
+      "",
+      "Starting date/time",
+      "! IBYR  = !",
+      "! IBMO  = !",
+      "! IBDY  = !",
+      "! IBHR  = !",
+      "! IBSEC = !",
+      "",
+      "Ending date/time",
+      "! IEYR  = !",
+      "! IEMO  = !",
+      "! IEDY  = !",
+      "! IEHR  = !",
+      "! IESEC = !",
+      "",
+      "File Options",
+      "! JDAT = 2 !",
+      "! ISUB = 2 !",
+      "! IFMT = 2 !",
+      "",
+      "Processing Options",
+      "! PSTOP = 500 !",
+      "! LXTOP = T   !",
+      "! PVTOP = 850 !",
+      "! LXSFC = T   !",
+      "! ZVSFC = 200 !",
+      "",
+      "!END!",
+      ""), "calmet_template.txt", sep = "\n")
+  
+}
