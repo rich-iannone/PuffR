35c46339fd5c367d50cfbf2c5134cb7f67c05248
rich-iannone <riannone@me.com>
2014-10-08 20:21:37 -0700

    Construct 'station_list' string based on requested station

--- R/download_FSL_sounding_data.R
+++ R/download_FSL_sounding_data.R
@@ -7,4 +7,15 @@ download_FSL_sounding_data <- function(sounding_priority,
                                        beginning_date,
                                        ending_date){
 
+  # Construct 'station_list' string based on requested station
+  station_list <- paste(df_soundings[station_list_position,1],
+                        df_soundings[station_list_position,2],
+                        df_soundings[station_list_position,3],
+                        df_soundings[station_list_position,4],
+                        df_soundings[station_list_position,5],
+                        sprintf("%05s", df_soundings[station_list_position,6]),
+                        str_replace_all(df_soundings[station_list_position,7], " ", "+"),
+                        df_soundings[station_list_position,8],
+                        df_soundings[station_list_position,9], sep = '+')
+  
 }
