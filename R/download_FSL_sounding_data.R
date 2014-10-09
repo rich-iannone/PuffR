1cd0d22b0ed27beecb967a93f3de9572b7da8c21
rich-iannone <riannone@me.com>
2014-10-08 20:20:44 -0700

    Create function 'download_FSL_sounding_data'

---
+++ R/download_FSL_sounding_data.R
@@ -0,0 +1,10 @@
+download_FSL_sounding_data <- function(sounding_priority,
+                                       df_soundings,
+                                       station_list_position,
+                                       starting_hour,
+                                       level_type,
+                                       wind_units,
+                                       beginning_date,
+                                       ending_date){
+
+}
