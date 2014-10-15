2ad48df064653177359a726fc07d5ed3a10caf8e
rich-iannone <riannone@me.com>
2014-10-14 17:06:15 -0700

    Construct filename for finalized READ62 input file

--- R/calmet_upper_air.R
+++ R/calmet_upper_air.R
@@ -518,6 +518,9 @@ calmet_upper_air <- function(location_name,
                                        keyword = "LCFILES",
                                        replacement = "T")
   
+  # Construct a filename for the finalized READ62 input file
+  read62_final_filename <- gsub("^up(.*)", "read62\\1", output_file)
+  
   #   ####
   #   # Process the upper air sounding data
   #   ####
