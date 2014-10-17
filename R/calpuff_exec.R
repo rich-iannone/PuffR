c1167395b1bbe1aeb34670c7e85a94fbae884103
rich-iannone <riannone@me.com>
2014-10-17 13:50:20 -0700

    Modify function to also accept the input file filename

--- R/calpuff_exec.R
+++ R/calpuff_exec.R
@@ -3,8 +3,6 @@
 #' @param calpuff_executable if the CALPUFF executable is in the system path, provide the name of the executable only; otherwise, provide the full path and name of the CALPUFF executable.
 #' @export calpuff_exec
 
-calpuff_exec <- function(calpuff_executable){
-  
   # Obtain list of CALPUFF input files
   calpuff_in_files <- list.files(pattern = "^calpuff_in")
   
@@ -16,6 +14,8 @@ calpuff_exec <- function(calpuff_executable){
                                           calpuff_executable, "' ", calpuff_in_files[i],
                                           sep = ""),
                           intern = TRUE)
+calpuff_exec <- function(calpuff_executable,
+                         calpuff_exec_file){
     
     # Write 'console_log' to a text file
     writeLines(console_log, con = paste("log_for_", calpuff_in_files[i], sep = '' ))
