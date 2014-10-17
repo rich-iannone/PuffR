5d91081bb47a3e1c70797069dbc5611b08b75234
rich-iannone <riannone@me.com>
2014-10-17 13:51:51 -0700

    Remove statements that obtain a list of CALPUFF input files

--- R/calpuff_exec.R
+++ R/calpuff_exec.R
@@ -4,12 +4,6 @@
 #' @param calpuff_exec_file the CALPUFF input file to execute.
 #' @export calpuff_exec
 
-  # Obtain list of CALPUFF input files
-  calpuff_in_files <- list.files(pattern = "^calpuff_in")
-  
-  # Process each input file with CALPUFF
-  for (i in 1:length(calpuff_in_files)){
-    
     # Run the input file with CALPUFF and capture output as lines to 'console_log'
     console_log <- system(command = paste("cd '", getwd(), "' ; '",
                                           calpuff_executable, "' ", calpuff_in_files[i],
