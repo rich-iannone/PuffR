a6912f3095090f4f11208e6870afb245aec0d334
rich-iannone <riannone@me.com>
2014-10-20 07:25:32 -0700

    Add arguments to 'calpuff_add_point_source'

--- R/calpuff_add_point_sources.R
+++ R/calpuff_add_point_sources.R
@@ -2,6 +2,23 @@
 #' @description Add point sources to a list for later use in CALPUFF
 #' @export calpuff_add_point_sources
 
-calpuff_add_point_sources <- function(){
+calpuff_add_point_sources <- function(src_name,
+                                      species_name,
+                                      lat,
+                                      lon,
+                                      x_coord_km,
+                                      y_coord_km,
+                                      UTM_zone,
+                                      stack_height,
+                                      base_elev,
+                                      stack_diam,
+                                      exit_velocity,
+                                      exit_temp,
+                                      emission_rate,
+                                      emission_units){
+  
+  
+  
+  
   
 }
