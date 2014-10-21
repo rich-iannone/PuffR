4a1f50060155f0a703180e0bcd561c4ab2cc758c
rich-iannone <riannone@me.com>
2014-10-21 16:57:13 -0700

    Modify object names

--- R/calpuff_add_point_sources.R
+++ R/calpuff_add_point_sources.R
@@ -70,7 +70,7 @@ calpuff_add_point_sources <- function(src_name,
   }
   
   # Determine whether lon/lat provided
-  if (!is.null(lat) & !is.null(lon)){
+  if (!is.null(lat_dec_deg) & !is.null(lon_dec_deg)){
     lon_lat_provided <- TRUE
   } else {
     lon_lat_provided <- FALSE
