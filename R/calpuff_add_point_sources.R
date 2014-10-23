95736c7b481428c6d7238e3cbf8507b935786210
rich-iannone <riannone@me.com>
2014-10-23 16:59:25 -0700

    Add 'UTM_hemisphere' to conditional block

--- R/calpuff_add_point_sources.R
+++ R/calpuff_add_point_sources.R
@@ -79,8 +79,9 @@ calpuff_add_point_sources <- function(src_name,
     lon_lat_provided <- FALSE
   }
   
-  # Determine whether UTM coordinates provided
-  if (!is.null(x_coord_km) & !is.null(y_coord_km) & !is.null(UTM_zone)){
+  # Determine whether UTM coordinates and zone information provided
+  if (!is.null(x_coord_km) & !is.null(y_coord_km)
+      & !is.null(UTM_zone) & !is.null(UTM_hemisphere)){
     UTM_provided <- TRUE
   } else {
     UTM_provided <- FALSE
