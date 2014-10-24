c389cfd5621c81a40562f97ee6dedd9511d92bc0
rich-iannone <riannone@me.com>
2014-10-23 20:18:09 -0700

    Add arguments to function

--- R/calpuff_add_area_sources.R
+++ R/calpuff_add_area_sources.R
@@ -15,7 +15,19 @@
 #' @param emission_units the units applied to the value defined in the 'emission_rate' argument. The possible selections are: (1) "g/m2/s", (2) "kg/m2/hr", (3) "lb/m2/hr", (4) "tons/m2/yr", (5) "Odour Unit * m/s", (6) "Odour Unit * m/min", (7) "metric tons/m2/yr", (8) "Bq/m2/s", and (9) "GBq/m2/yr".
 #' @export calpuff_add_area_sources
 
-calpuff_add_area_sources <- function(){
+calpuff_add_area_sources <- function(src_name,
+                                     species_name,
+                                     lat_dec_deg = NULL,
+                                     lon_dec_deg = NULL,
+                                     x_coord_km = NULL,
+                                     y_coord_km = NULL,
+                                     UTM_zone = NULL,
+                                     UTM_hemisphere = NULL,
+                                     effective_height,
+                                     base_elev,
+                                     init_sigma_z,
+                                     emission_rate,
+                                     emission_units){
   
   # Add require statements
   require(rgdal)
