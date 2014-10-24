d1e65915270ff5a24d501c855199ba45eabcec9c
rich-iannone <riannone@me.com>
2014-10-24 15:43:23 -0700

    Define the UTM y coordinate in km units

--- R/calpuff_add_area_sources.R
+++ R/calpuff_add_area_sources.R
@@ -110,4 +110,9 @@ calpuff_add_area_sources <- function(src_name,
     # Define the UTM x coordinate in km units
     x_coord_km <- UTM_location[1,1] / 1000
     
+    # Define the UTM y coordinate in km units
+    y_coord_km <- UTM_location[1,2] / 1000
+    
+  }
+  
 }
