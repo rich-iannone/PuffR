1f7df5a4fc0bd7d3d306f36aed6bba3ad8d5cd8d
rich-iannone <riannone@me.com>
2014-10-25 13:20:35 -0700

    Add roxygen @param statements

--- R/calpuff_add_line_sources.R
+++ R/calpuff_add_line_sources.R
@@ -1,5 +1,13 @@
 #' Add line sources to a list for later use in CALPUFF
 #' @description Add line sources to a list for later use in CALPUFF
+#' @param src_name the name of the source emitting the species.
+#' @param species_name the name of the species undergoing emissions.
+#' @param lat_dec_deg the latitude of the line source in decimal degrees.
+#' @param lon_dec_deg the longitude of the line source in decimal degrees.
+#' @param x_coord_km the UTM easting value of the line source in km units.
+#' @param y_coord_km the UTM northing value of the line source in km units.
+#' @param UTM_zone the UTM zone for the line source.
+#' @param UTM_hemisphere the UTM hemisphere for the line source.
 #' @export calpuff_add_line_sources
 
 calpuff_add_line_sources <- function(src_name,
