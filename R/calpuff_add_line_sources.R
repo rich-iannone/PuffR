e757a2ecf95915f831fe55fe27d85ec51fbcb055
rich-iannone <riannone@me.com>
2014-10-25 13:20:21 -0700

    Add arguments and default values to function

--- R/calpuff_add_line_sources.R
+++ R/calpuff_add_line_sources.R
@@ -2,6 +2,13 @@
 #' @description Add line sources to a list for later use in CALPUFF
 #' @export calpuff_add_line_sources
 
-calpuff_add_line_sources <- function(){
+calpuff_add_line_sources <- function(src_name,
+                                     species_name,
+                                     lat_dec_deg = NULL,
+                                     lon_dec_deg = NULL,
+                                     x_coord_km = NULL,
+                                     y_coord_km = NULL,
+                                     UTM_zone = NULL,
+                                     UTM_hemisphere = NULL){
   
 }
