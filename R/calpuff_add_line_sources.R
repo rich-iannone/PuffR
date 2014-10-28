679b00d30fd13e60ee27e4f457d668b67f43b85d
rich-iannone <riannone@me.com>
2014-10-28 06:53:01 -0700

    Modify several roxygen @param statements

--- R/calpuff_add_line_sources.R
+++ R/calpuff_add_line_sources.R
@@ -2,21 +2,13 @@
 #' @description Add line sources to a list for later use in CALPUFF
 #' @param src_name the name of the source emitting the species.
 #' @param species_name the name of the species undergoing emissions.
-#' @param lat_dec_deg_1 the latitude of point 1 of the line source in decimal degrees.
-#' @param lat_dec_deg_2 the latitude of point 2 of the line source in decimal degrees.
-#' @param lon_dec_deg_1 the longitude of point 1 of the line source in decimal degrees.
-#' @param lon_dec_deg_2 the longitude of point 2 of the line source in decimal degrees.
-#' @param x_coord_km_1 the UTM easting value of point 1 of the line source in km units.
-#' @param x_coord_km_2 the UTM easting value of point 2 of the line source in km units.
-#' @param y_coord_km_1 the UTM northing value of point 1 of the line source in km units.
-#' @param y_coord_km_2 the UTM northing value of point 2 of the line source in km units.
+#' @param lat_dec_deg a vector of 2 latitude values for the line source in units of decimal degrees.
+#' @param lon_dec_deg a vector of 2 longitude values for the line source in units of decimal degrees.
+#' @param x_coord_km a vector of 2 UTM easting values for the line source in km units.
+#' @param y_coord_km a vector of 2 UTM northing values for the area source in km units.
 #' @param UTM_zone the UTM zone for the line source.
 #' @param UTM_hemisphere the UTM hemisphere for the line source.
-#' @param beg_x_coord 
-#' @param beg_y_coord 
-#' @param end_x_coord 
-#' @param end_y_coord 
-#' @param release_hgt 
+#' @param release_hgt the release height of the line source in meters above ground level (m AGL).
 #' @param base_elev the ground elevation at the location of the area source in meters above sea level (m ASL).
 #' @param emission_rate the rate of constant emissions from the line source; units are defined in the 'emission_units' argument.
 #' @param emission_units the units applied to the value defined in the 'emission_rate' argument. The possible selections are: (1) "g/s", (2) "kg/hr", (3) "lb/hr", (4) "tons/yr", (5) "Odour Unit * m3/s", (6) "Odour Unit * m3/min", (7) "metric tons/yr", (8) "Bq/s", and (9) "GBq/yr".
