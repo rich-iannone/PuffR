b9374486f66e9d8376c03b452359c5a0f6e2f06b
rich-iannone <riannone@me.com>
2014-10-28 06:52:36 -0700

    Add descriptions to 2 roxygen @param statements

--- R/calpuff_add_area_sources.R
+++ R/calpuff_add_area_sources.R
@@ -8,9 +8,9 @@
 #' @param y_coord_km a vector of 4 UTM northing values for the area source in km units.
 #' @param UTM_zone the UTM zone for the area source.
 #' @param UTM_hemisphere the UTM hemisphere for the area source.
-#' @param effective_height
+#' @param effective_height the effective height of the area source in meters above ground level (m AGL).
 #' @param base_elev the ground elevation at the location of the area source in meters above sea level (m ASL).
-#' @param init_sigma_z
+#' @param init_sigma_z the initial sigma z value for the area source in meters.
 #' @param emission_rate the rate of constant emissions from the area source; units are defined in the 'emission_units' argument.
 #' @param emission_units the units applied to the value defined in the 'emission_rate' argument. The possible selections are: (1) "g/m2/s", (2) "kg/m2/hr", (3) "lb/m2/hr", (4) "tons/m2/yr", (5) "Odour Unit * m/s", (6) "Odour Unit * m/min", (7) "metric tons/m2/yr", (8) "Bq/m2/s", and (9) "GBq/m2/yr".
 #' @export calpuff_add_area_sources
