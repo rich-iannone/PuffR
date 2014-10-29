a6472e2af0ee4b08094c12a388a8802acad09288
rich-iannone <riannone@me.com>
2014-10-29 06:51:16 -0700

    Add roxygen @param statements for 2 arguments

--- R/calpuff_add_volume_sources.R
+++ R/calpuff_add_volume_sources.R
@@ -1,5 +1,7 @@
 #' Add volume sources to a list for later use in CALPUFF
 #' @description Add volume sources to a list for later use in CALPUFF
+#' @param src_name the name of the source emitting the species.
+#' @param species_name the name of the species undergoing emissions.
 #' @export calpuff_add_volume_sources
 
 calpuff_add_volume_sources <- function(src_name,
