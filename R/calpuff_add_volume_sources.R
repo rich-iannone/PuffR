3f55118a38bf73ebe4fb04c53d8f4be1f76c1480
rich-iannone <riannone@me.com>
2014-10-30 12:19:49 -0700

    Add roxygen @param statements

--- R/calpuff_add_volume_sources.R
+++ R/calpuff_add_volume_sources.R
@@ -2,6 +2,8 @@
 #' @description Add volume sources to a list for later use in CALPUFF
 #' @param src_name the name of the source emitting the species.
 #' @param species_name the name of the species undergoing emissions.
+#' @param emission_rate the rate of constant emissions from the line source; units are defined in the 'emission_units' argument.
+#' @param emission_units the units applied to the value defined in the 'emission_rate' argument. The possible selections are: (1) "g/s", (2) "kg/hr", (3) "lb/hr", (4) "tons/yr", (5) "Odour Unit * m3/s", (6) "Odour Unit * m3/min", (7) "metric tons/yr", (8) "Bq/s", and (9) "GBq/yr".
 #' @export calpuff_add_volume_sources
 
 calpuff_add_volume_sources <- function(src_name,
