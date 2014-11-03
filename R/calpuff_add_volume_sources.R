6f8310a14d079adcb3e3a64ec8ef5b42d3cb49a9
rich-iannone <riannone@me.com>
2014-11-03 07:03:21 -0800

    Determine the hemisphere of the source location

--- R/calpuff_add_volume_sources.R
+++ R/calpuff_add_volume_sources.R
@@ -97,4 +97,7 @@ calpuff_add_volume_sources <- function(src_name,
     # Determine the UTM zone
     UTM_zone <- (floor((lon_dec_deg + 180)/6) %% 60) + 1
     
+    # Determine whether source is in the Northern Hemisphere or the Southern Hemisphere
+    UTM_hemisphere <- ifelse(lat_dec_deg >= 0, "N", "S")
+    
 }
