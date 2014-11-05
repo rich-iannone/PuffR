710c7d38f2875309da095be6cd465ebbe88d08a5
rich-iannone <riannone@me.com>
2014-11-05 09:53:27 -0800

    Create function 'calpuff_create_varying_area_sources'

---
+++ R/calpuff_create_varying_area_sources.R
@@ -0,0 +1,7 @@
+#' Create CALPUFF input file with area sources that vary in location and time
+#' @description Create CALPUFF input file with area sources that vary in location and time.
+#' @export calpuff_create_varying_area_sources
+
+calpuff_create_varying_area_sources <- function(){
+  
+}
