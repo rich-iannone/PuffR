55dd568c83d01643609e2a2be39bae8e5947e17e
rich-iannone <riannone@me.com>
2014-11-04 18:28:50 -0800

    Create function 'calpuff_create_varying_point_sources'

---
+++ R/calpuff_create_varying_point_sources.R
@@ -0,0 +1,7 @@
+#' Create CALPUFF input file with point sources that vary in location and time
+#' @description Create CALPUFF input file with point sources that vary in location and time.
+#' @export calpuff_create_varying_point_sources
+
+calpuff_create_varying_point_sources <- function(){
+  
+}
