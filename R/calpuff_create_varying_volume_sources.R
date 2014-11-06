a3e97d621cbeadf2d0f0a21d250671d8763fbcde
rich-iannone <riannone@me.com>
2014-11-06 06:27:04 -0800

    Add argument 'CSV_input'

--- R/calpuff_create_varying_volume_sources.R
+++ R/calpuff_create_varying_volume_sources.R
@@ -2,6 +2,6 @@
 #' @description Create CALPUFF input file with volume sources that vary in location and time.
 #' @export calpuff_create_varying_volume_sources
 
-calpuff_create_varying_volume_sources <- function(){
+calpuff_create_varying_volume_sources <- function(CSV_input){
   
 }
