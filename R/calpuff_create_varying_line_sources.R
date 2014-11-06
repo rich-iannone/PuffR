635d900b9f4e7679a89c014f4d6c15de466ddae0
rich-iannone <riannone@me.com>
2014-11-05 20:24:30 -0800

    Add function 'calpuff_create_varying_line_sources'

---
+++ R/calpuff_create_varying_line_sources.R
@@ -0,0 +1,8 @@
+#' Create CALPUFF input file with line sources that vary in location and time
+#' @description Create CALPUFF input file with line sources that vary in location and time.
+#' @export calpuff_create_varying_line_sources
+
+calpuff_create_varying_line_sources <- function(){
+  
+}
+
