450fc324121ee37f18be989c16ed3db546876f1b
rich-iannone <riannone@me.com>
2014-11-06 22:37:42 -0800

    Add two arguments to function with NULL as default

--- R/calpuff_create_varying_point_sources.R
+++ R/calpuff_create_varying_point_sources.R
@@ -2,6 +2,7 @@
 #' @description Create CALPUFF input file with point sources that vary in location and time.
 #' @export calpuff_create_varying_point_sources
 
-calpuff_create_varying_point_sources <- function(){
+calpuff_create_varying_point_sources <- function(CSV_input = NULL,
+                                                 df_input = NULL){
   
 }
