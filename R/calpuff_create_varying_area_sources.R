e04a290fd75b781258700a44aefc9d6f5b36955b
rich-iannone <riannone@me.com>
2014-11-06 22:37:34 -0800

    Add two arguments to function with NULL as default

--- R/calpuff_create_varying_area_sources.R
+++ R/calpuff_create_varying_area_sources.R
@@ -2,6 +2,7 @@
 #' @description Create CALPUFF input file with area sources that vary in location and time.
 #' @export calpuff_create_varying_area_sources
 
-calpuff_create_varying_area_sources <- function(){
+calpuff_create_varying_area_sources <- function(CSV_input = NULL,
+                                                df_input = NULL){
   
 }
