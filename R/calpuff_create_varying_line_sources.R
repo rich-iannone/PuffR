7f09aade828934ef759e94299590fdaacdf966ae
rich-iannone <riannone@me.com>
2014-11-06 22:37:37 -0800

    Add two arguments to function with NULL as default

--- R/calpuff_create_varying_line_sources.R
+++ R/calpuff_create_varying_line_sources.R
@@ -2,7 +2,8 @@
 #' @description Create CALPUFF input file with line sources that vary in location and time.
 #' @export calpuff_create_varying_line_sources
 
-calpuff_create_varying_line_sources <- function(){
+calpuff_create_varying_line_sources <- function(CSV_input = NULL,
+                                                df_input = NULL){
   
 }
 
