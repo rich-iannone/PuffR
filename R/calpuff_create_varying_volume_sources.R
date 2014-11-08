f90a5e5daa8e6e6b02417ec0065030c88721dbee
rich-iannone <riannone@me.com>
2014-11-08 08:34:23 -0800

    Add two arguments to function

--- R/calpuff_create_varying_volume_sources.R
+++ R/calpuff_create_varying_volume_sources.R
@@ -3,6 +3,8 @@
 #' @export calpuff_create_varying_volume_sources
 
 calpuff_create_varying_volume_sources <- function(CSV_input = NULL,
-                                                  df_input = NULL){
+                                                  df_input = NULL,
+                                                  src_name,
+                                                  species_name){
   
 }
