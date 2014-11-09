d03949c4eda50704130327da4fd6ae7b910cab46
rich-iannone <riannone@me.com>
2014-11-09 00:10:42 -0800

    Add several roxygen @param statements

--- R/calpuff_create_varying_point_sources.R
+++ R/calpuff_create_varying_point_sources.R
@@ -1,5 +1,9 @@
 #' Create CALPUFF input file with point sources that vary in location and time
 #' @description Create CALPUFF input file with point sources that vary in location and time.
+#' @param CSV_input a path to a CSV file containing hourly point source data.
+#' @param df_input a data frame containing hourly point source data.
+#' @param src_name the name of the source emitting the species.
+#' @param species_name the name of the species undergoing emissions.
 #' @export calpuff_create_varying_point_sources
 
 calpuff_create_varying_point_sources <- function(CSV_input = NULL,
