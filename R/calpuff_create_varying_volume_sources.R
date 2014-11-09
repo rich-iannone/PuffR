0c849690910d4e4319df5b51d9190fcc4d5212aa
rich-iannone <riannone@me.com>
2014-11-09 00:10:46 -0800

    Add several roxygen @param statements

--- R/calpuff_create_varying_volume_sources.R
+++ R/calpuff_create_varying_volume_sources.R
@@ -1,5 +1,9 @@
 #' Create CALPUFF input file with volume sources that vary in location and time
 #' @description Create CALPUFF input file with volume sources that vary in location and time.
+#' @param CSV_input a path to a CSV file containing hourly volume source data.
+#' @param df_input a data frame containing hourly volume source data.
+#' @param src_name the name of the source emitting the species.
+#' @param species_name the name of the species undergoing emissions.
 #' @export calpuff_create_varying_volume_sources
 
 calpuff_create_varying_volume_sources <- function(CSV_input = NULL,
