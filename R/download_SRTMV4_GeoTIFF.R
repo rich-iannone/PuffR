43b8018a8456f94277737e63091656c3781730c1
rich-iannone <riannone@me.com>
2014-11-21 15:35:09 -0800

    Move statement to earlier part of script

--- R/download_SRTMV4_GeoTIFF.R
+++ R/download_SRTMV4_GeoTIFF.R
@@ -29,8 +29,7 @@ download_SRTMV4_GeoTIFF <- function(lon,
   stopifnot(lon >= -180 & lon <= 180)
   stopifnot(lat >= -60 & lat <= 60)
   
-  rs <- raster(nrows = 24, ncols = 72, xmn = -180, xmx = 180, 
-               ymn = -60, ymx = 60)
+
   
   rowTile <- rowFromY(rs, lat)
   colTile <- colFromX(rs, lon)
