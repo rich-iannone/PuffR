c3901da654808da2355fc942f0ac5310d24441ed
rich-iannone <riannone@me.com>
2014-11-20 21:07:08 -0800

    Obtain text lines of SURF.DAT file as a vector object

--- R/calpuff_create_varying_area_sources.R
+++ R/calpuff_create_varying_area_sources.R
@@ -30,6 +30,9 @@ calpuff_create_varying_area_sources <- function(CSV_input = NULL,
   # Use 'domain_dimensions' to get the first item from a vector list of SURF.DAT files
   surf_dat_file <- list.files(pattern = paste0("surf--.*?-", domain_dimensions, ".*"))[1]
   
+  # Obtain text lines of SURF.DAT file as a vector object
+  surf_dat_lines <- readLines(surf_dat_file, warn = FALSE)
+  
   # Change 'date_time' column to 'POSIXct' class
   if (class(area_sources_df$date_time)[1] == "POSIXct"){
     NULL
