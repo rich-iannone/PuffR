8fc05c2e4e57e19bf0bb4cd65b784129038b6c63
rich-iannone <riannone@me.com>
2014-11-28 06:50:07 -0800

    Modify indentation

--- R/calpuff_04_grid_levels.R
+++ R/calpuff_04_grid_levels.R
@@ -31,40 +31,40 @@
 #' @export calpuff_04_grid_levels
 
 calpuff_04_grid_levels <- function(calpuff_inp,
-                                  pmap = "UTM",
-                                  feast = 0.0,
-                                  fnorth = 0.0,
-                                  iutmzn = NULL,
-                                  utmhem = NULL,
-                                  rlat0 = "40N",
-                                  rlon0 = "90W",
-                                  xlat1 = "30N",
-                                  xlat2 = "60N",
-                                  datum = "WGS-84",
-                                  nx = NULL,
-                                  ny = NULL,
-                                  dgridkm = NULL,
-                                  xorigkm = NULL,
-                                  yorigkm = NULL,
-                                  nz = 12,
-                                  zface = c(0,20,40,80,100,150,200,300,400,800,1400,2000,3000),
-                                  ibcomp,
-                                  jbcomp,
-                                  iecomp,
-                                  jecomp,
-                                  lsamp,
-                                  ibsamp,
-                                  jbsamp,
-                                  iesamp,
-                                  jesamp,
-                                  meshdn){
-
+                                   pmap = "UTM",
+                                   feast = 0.0,
+                                   fnorth = 0.0,
+                                   iutmzn = NULL,
+                                   utmhem = NULL,
+                                   rlat0 = "40N",
+                                   rlon0 = "90W",
+                                   xlat1 = "30N",
+                                   xlat2 = "60N",
+                                   datum = "WGS-84",
+                                   nx = NULL,
+                                   ny = NULL,
+                                   dgridkm = NULL,
+                                   xorigkm = NULL,
+                                   yorigkm = NULL,
+                                   nz = 12,
+                                   zface = c(0,20,40,80,100,150,200,300,400,800,1400,2000,3000),
+                                   ibcomp,
+                                   jbcomp,
+                                   iecomp,
+                                   jecomp,
+                                   lsamp,
+                                   ibsamp,
+                                   jbsamp,
+                                   iesamp,
+                                   jesamp,
+                                   meshdn){
+  
   # Generate a vector list of calmet.inp keywords
   keywords <- c("PMAP", "FEAST", "FNORTH", "IUTMZN", "UTMHEM", "RLAT0", "RLON0", 
                 "XLAT1", "XLAT2", "DATUM", "NX", "NY", "DGRIDKM", "XORIGKM", "YORIGKM", 
                 "NZ", "ZFACE", "IBCOMP", "JBCOMP", "IECOMP", "JECOMP", "LSAMP",
                 "IBSAMP", "JBSAMP", "IESAMP", "JESAMP", "MESHDN")
-
+  
   # Generate a vector list of the formatted replacements
   replacements <- c(pmap, feast, fnorth, iutmzn, utmhem, rlat0, rlon0, 
                     xlat1, xlat2, datum, nx, ny, dgridkm, xorigkm, yorigkm, 
@@ -78,7 +78,5 @@ calpuff_04_grid_levels <- function(calpuff_inp,
   
   # Write the output to the same working calmet.inp file
   writeLines(calpuff_inp_working, con = calpuff_inp)
-  
-  
   
 }
