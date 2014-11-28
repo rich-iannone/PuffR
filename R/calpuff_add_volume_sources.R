f54674b544891d7a9f23f18316dee7131d1890b8
rich-iannone <riannone@me.com>
2014-11-28 07:06:21 -0800

    Replace call to 'paste' with 'paste0'

--- R/calpuff_add_volume_sources.R
+++ R/calpuff_add_volume_sources.R
@@ -41,10 +41,10 @@ calpuff_add_volume_sources <- function(src_name,
   
   # Get expected filename for volume sources
   vol_sources_filename <-
-    paste(unlist(str_split(getwd(),
-                           pattern = "/"))[length(unlist(str_split(getwd(),
-                                                                   pattern = "/")))],
-          "--volume_sources.txt", sep = '')
+    paste0(unlist(str_split(getwd(),
+                            pattern = "/"))[length(unlist(str_split(getwd(),
+                                                                    pattern = "/")))],
+           "--volume_sources.txt")
   
   # Create volume sources text file with header if it doesn't exist
   if (file.exists(vol_sources_filename) == FALSE){
