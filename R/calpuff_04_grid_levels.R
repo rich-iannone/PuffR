f6eb31669d8598a53de4fb1b0ba5c50d6ef85f81
rich-iannone <riannone@me.com>
2014-12-05 13:11:46 -0800

    Add default value to 'calpuff_inp' argument

--- R/calpuff_04_grid_levels.R
+++ R/calpuff_04_grid_levels.R
@@ -30,7 +30,7 @@
 #' @param meshdn 
 #' @export calpuff_04_grid_levels
 
-calpuff_04_grid_levels <- function(calpuff_inp,
+calpuff_04_grid_levels <- function(calpuff_inp = "calpuff_template.txt",
                                    pmap = "UTM",
                                    feast = 0.0,
                                    fnorth = 0.0,
