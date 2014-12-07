098bec60830ba044b3aed2d9ca13f847a6976168
rich-iannone <riannone@me.com>
2014-12-06 18:33:19 -0800

    Add information to roxygen @param statement

--- R/calpuff_08_size_ddep_particles.R
+++ R/calpuff_08_size_ddep_particles.R
@@ -1,6 +1,6 @@
 #' Set the CALPUFF size parameters for dry deposition of particles
 #' @description This function validates and writes CALPUFF size parameters for dry deposition of particles.
-#' @param calpuff_inp 
+#' @param calpuff_inp the absolute path and filename for the working CALPUFF input file.
 #' @param species_geom_mass_mean_diam 
 #' @param species_geom_SD 
 #' @export calpuff_08_size_ddep_particles
