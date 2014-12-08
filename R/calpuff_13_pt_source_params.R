9b4bc76a6f2281710b36ae74143430acaba64ac0
rich-iannone <riannone@me.com>
2014-12-08 06:59:44 -0800

    Add information to several roxygen @param statements

--- R/calpuff_13_pt_source_params.R
+++ R/calpuff_13_pt_source_params.R
@@ -1,12 +1,12 @@
 #' Set one or more CALPUFF point source parameters
 #' @description This function validates and writes CALPUFF point source parameters.
 #' @param calpuff_inp the absolute path and filename for the working CALPUFF input file.
-#' @param npt1 
-#' @param iptu 
-#' @param nspt1
-#' @param npt2
-#' @param ptsource_name 
-#' @param ptsource_params 
+#' @param npt1 the number of point sources.
+#' @param iptu the units used for line source emissions, where the possible selections are: (1) "g/s", (2) "kg/hr", (3) "lb/hr", (4) "tons/yr", (5) "Odour Unit * m^3/s", (6) "Odour Unit * m^3/min", and (7) "metric tons/yr".
+#' @param nspt1 the number of source-species combinations with variable emissions scaling factors.
+#' @param npt2 the number of point sources with variable location and emission parameters.
+#' @param ptsource_name a vector of assigned names for each point source.
+#' @param ptsource_params a list of point source parameters for each source in the order of: (1) x UTM coordinate (km), (2) y UTM coordinate (km), (3) stack height (m), (4) base elevation (m), (5) stack diameter (m), (6) exit velocity (m/s), (7) exit temperature (K), (8) building downwash computed (where 1 is yes, 0 is no), (9) emission rates for each species emitted from point source in units specified in the 'iptu' argument. 
 #' @param ptsource_zpltfm 
 #' @param ptsource_fmfac 
 #' @param downwash_output 
