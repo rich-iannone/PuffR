f59edbe44b53470f783318b371ffa5d9bac1bd47
rich-iannone <riannone@me.com>
2014-12-15 01:19:53 -0800

    Modify several roxygen @param statements

--- R/calpuff_01_run_control_params.R
+++ R/calpuff_01_run_control_params.R
@@ -1,7 +1,7 @@
 #' Set CALPUFF run control parameters
 #' @description This function validates and writes CALPUFF run control parameters.
 #' @param calpuff_inp the absolute path and filename for the working CALPUFF input file.
-#' @param metrun 
+#' @param metrun the choice of whether to run all periods available in meteorological input file.
 #' @param ibyr the starting year for the CALPUFF run.
 #' @param ibmo the starting month for the CALPUFF run.
 #' @param ibdy the starting day for the CALPUFF run.
@@ -18,16 +18,17 @@
 #' @param itest a flag to stop the model run after the setup phase, which is useful for checking for correctness in model inputs and associated files.
 #' @param mrestart a flag to interact with restart files.
 #' @param nrespd the number of periods in the restart output cycle.
-#' @param metfm the meteorological data format to be used.
-#' @param mprffm 
+#' @param metfm the meteorological data format to be used: (1) CALMET binary file (CALMET.DAT), (2) ISC ASCII file (ISCMET.DAT), (3) AUSPLUME ASCII file (PLMMET.MET), (4) CTDM plus tower file (PROFILE.DAT) and surface parameters file (SURFACE.DAT), or (5) AERMET tower file (PROFILE.DAT) and surface parameters file (SURFACE.DAT).
+#' @param mprffm the meteorological profile data format (ignored unless metfm is either 1, 2, or 3): (1) CTDM plus tower file, or (2) AERMET tower file.
 #' @param avet the averaging time in minutes.
 #' @param pgtime the PG averaging time in minutes.
-#' @param ioutu 
-#' @param iovers
+#' @param ioutu the output units for binary concentration and flux files: (1) mass units (e.g., g/m3 or g/m2/s), (2) odour units, or (3) radiation units (e.g., Bq/m3 or Bq/m2/s).
+#' @param iovers the output dataset format version for binary concentration and flux files: (1) version 2.1, or (2) version 2.2.
 #' @export calpuff_01_run_control_params
 
 calpuff_01_run_control_params <- function(calpuff_inp = "calpuff_template.txt",
                                           read_data_from_surf_dat = TRUE,
+                                          metrun = TRUE,
                                           ibyr = NULL,
                                           ibmo = NULL,
                                           ibdy = NULL,
