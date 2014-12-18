f0190b02cc789f85c41da28367a167585a96991c
rich-iannone <riannone@me.com>
2014-12-18 08:20:34 -0800

    Transform TRUE or FALSE value for 'msplit' to numeric

--- R/calpuff_02_technical_opts.R
+++ R/calpuff_02_technical_opts.R
@@ -100,6 +100,12 @@ calpuff_02_technical_opts <- function(calpuff_inp = "calpuff_template.txt",
     mshear <- 0
   }
   
+  # Transform TRUE or FALSE value for 'msplit' to a numeric value
+  if (msplit == TRUE){
+    msplit <- 1
+  } else if (msplit == FALSE){
+    msplit <- 0
+  }
   
   # Generate a vector list of calpuff.inp keywords
   keywords <- c("MGAUSS", "MCTADJ", "MCTSG", "MSLUG", "MTRANS", "MTIP", "MRISE", 
