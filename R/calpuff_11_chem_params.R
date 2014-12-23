dee6a4a81b3b06dd13fa8a1fd587c01a7a5bd7e2
rich-iannone <riannone@me.com>
2014-12-23 12:14:16 -0800

    Add default values to arguments

--- R/calpuff_11_chem_params.R
+++ R/calpuff_11_chem_params.R
@@ -20,22 +20,26 @@
 #' @export calpuff_11_chem_params
 
 calpuff_11_chem_params <- function(calpuff_inp = "calpuff_template.txt",
-                                   moz,
-                                   bcko3,
-                                   mnh3,
-                                   mavgnh3,
-                                   bcknh3,
-                                   rnite1,
-                                   rnite2,
-                                   rnite3,
-                                   mh2o2,
-                                   bckh2o2,
-                                   bckpmf,
-                                   ofrac,
-                                   vcnx,
-                                   ndecay,
-                                   spec1,
-                                   spec2){
+                                   moz = 1,
+                                   bcko3 = c(40.0, 40.0, 40.0, 40.0, 40.0, 40.0,
+                                             40.0, 40.0, 40.0, 40.0, 40.0, 40.0),
+                                   mnh3 = 0,
+                                   mavgnh3 = 1,
+                                   bcknh3 = c(10.0, 10.0, 10.0, 10.0, 10.0, 10.0,
+                                              10.0, 10.0, 10.0, 10.0, 10.0, 10.0),
+                                   rnite1 = 0.2,
+                                   rnite2 = 2.0,
+                                   rnite3 = 2.0,
+                                   mh2o2 = 1,
+                                   bckh2o2 = c(1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+                                               1.0, 1.0, 1.0, 1.0, 1.0, 1.0),
+                                   bckpmf = c(1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+                                              1.0, 1.0, 1.0, 1.0, 1.0, 1.0),
+                                   ofrac = c(0.15, 0.15, 0.20, 0.20, 0.20, 0.20,
+                                             0.20, 0.20, 0.20, 0.20, 0.20, 0.15),
+                                   vcnx = c(50.0, 50.0, 50.0, 50.0, 50.0, 50.0,
+                                            50.0, 50.0, 50.0, 50.0, 50.0, 50.0),
+                                   ndecay = 0){
   
   # Read in the working calpuff.inp file as a character vector
   calpuff_inp_working <- readLines(calpuff_inp, warn = FALSE)
