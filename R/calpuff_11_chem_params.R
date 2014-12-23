a802aefe61664f63be4d06dcdd9717cd1ff3ce51
rich-iannone <riannone@me.com>
2014-12-23 12:15:06 -0800

    Remove objects from vector lists

--- R/calpuff_11_chem_params.R
+++ R/calpuff_11_chem_params.R
@@ -44,11 +44,11 @@ calpuff_11_chem_params <- function(calpuff_inp = "calpuff_template.txt",
   
   # Generate a vector list of calmet.inp keywords
   keywords <- c("MOZ", "BCKO3", "MNH3", "MAVGNH3", "BCKNH3", "RNITE1", "RNITE2", "RNITE3",
-                "MH2O2", "BCKH2O2", "BCKPMF", "OFRAC", "VCNX", "NDECAY", "SPEC1", "SPEC2")
+                "MH2O2", "BCKH2O2", "BCKPMF", "OFRAC", "VCNX", "NDECAY")
   
   # Generate a vector list of the formatted replacements
   replacements <- c(moz, bcko3, mnh3, mavgnh3, bcknh3, rnite1, rnite2, rnite3,
-                    mh2o2, bckh2o2, bckpmf, ofrac, vcnx, ndecay, spec1, spec2)
+                    mh2o2, bckh2o2, bckpmf, ofrac, vcnx, ndecay)
   
   # Modify all parameters in working calmet.inp vector
   calpuff_inp_working <- replace_in_inp(inp_file_working = calpuff_inp_working,
