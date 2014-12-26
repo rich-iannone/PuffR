f0c451413459e56bc0d49cc10440b938b6d75830
rich-iannone <riannone@me.com>
2014-12-25 23:17:15 -0800

    Add two arguments to function

--- R/calpuff_07_chem_ddep_gases.R
+++ R/calpuff_07_chem_ddep_gases.R
@@ -4,6 +4,8 @@
 #' @export calpuff_07_chem_ddep_gases
 
 calpuff_07_chem_ddep_gases <- function(calpuff_inp = "calpuff_template.txt",
+                                       species_names = NULL,
+                                       species_ddep_params = NULL){
   
   # Read in the working calpuff.inp file as a character vector
   calpuff_inp_working <- readLines(calpuff_inp, warn = FALSE)
