5c63b7bad00a2d9a321db61c1242fc1516beb224
rich-iannone <riannone@me.com>
2014-12-25 23:16:56 -0800

    Remove several roxygen @param statements

--- R/calpuff_07_chem_ddep_gases.R
+++ R/calpuff_07_chem_ddep_gases.R
@@ -1,11 +1,6 @@
 #' Set the CALPUFF chemical parameters for dry deposition of gases
 #' @description This function validates and writes CALPUFF chemical parameters for dry deposition of gases.
 #' @param calpuff_inp the absolute path and filename for the working CALPUFF input file.
-#' @param species_diffusivity 
-#' @param species_alpha_star 
-#' @param species_reactivity 
-#' @param species_mesophyll_resistance 
-#' @param species_HL_coefficient 
 #' @export calpuff_07_chem_ddep_gases
 
 calpuff_07_chem_ddep_gases <- function(calpuff_inp = "calpuff_template.txt",
