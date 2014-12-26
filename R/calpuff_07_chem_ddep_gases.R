6b9d2697ff1713ea6905384280600ef202a8afda
rich-iannone <riannone@me.com>
2014-12-25 23:20:17 -0800

    Add two roxygen @param statements

--- R/calpuff_07_chem_ddep_gases.R
+++ R/calpuff_07_chem_ddep_gases.R
@@ -1,6 +1,8 @@
 #' Set the CALPUFF chemical parameters for dry deposition of gases
 #' @description This function validates and writes CALPUFF chemical parameters for dry deposition of gases.
 #' @param calpuff_inp the absolute path and filename for the working CALPUFF input file.
+#' @param species_names a vector list of gas-phase chemical species for which dry deposition parameters are available.
+#' @param species_ddep_params a vector list of the following chemical parameters for dry deposition of gases: (1) alpha star, (2) reactivity, (3) mesophyll resistance, and (4) Henry's Law coefficient.
 #' @export calpuff_07_chem_ddep_gases
 
 calpuff_07_chem_ddep_gases <- function(calpuff_inp = "calpuff_template.txt",
