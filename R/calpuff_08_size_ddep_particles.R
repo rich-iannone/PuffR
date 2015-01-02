bf2f2d593361720a6f3746a84b760970d4715e5a
rich-iannone <riannone@me.com>
2015-01-02 00:33:00 -0800

    Add default params for several chemical species

--- R/calpuff_08_size_ddep_particles.R
+++ R/calpuff_08_size_ddep_particles.R
@@ -9,6 +9,14 @@ calpuff_08_size_ddep_particles <- function(calpuff_inp = "calpuff_template.txt",
                                            species_geom_mass_mean_diam = NULL,
                                            species_geom_SD = NULL){
   
+  # Generate default list of values for the geometric mass mean diameter and the
+  # geometric standard deviation (both in units of microns) for a large number of
+  # chemical species
+  default_params_ddep <- list(so4 = c(0.48, 2.0),
+                              no3 = c(0.48, 2.0),
+                              pm25 = c(0.48, 2.0)
+                              pm10 = c(0.48, 2.0))
+  
   # Read in the working calpuff.inp file as a character vector
   calpuff_inp_working <- readLines(calpuff_inp, warn = FALSE)
   
