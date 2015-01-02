750bd4c12b10077525272fd7b8cc987544e4535e
rich-iannone <riannone@me.com>
2015-01-02 00:33:05 -0800

    Add default params for several chemical species

--- R/calpuff_10_wdep_params.R
+++ R/calpuff_10_wdep_params.R
@@ -9,4 +9,16 @@ calpuff_10_wdep_params <- function(calpuff_inp = "calpuff_template.txt",
                                    species_scaveng_coeff_liquid_precip = NULL,
                                    species_scaveng_coeff_froze_precip = NULL){
   
+  # Generate default list of values for the scavenging coefficients when wet
+  # deposition occurs via either liquid or frozen precipitation for a large number
+  # of chemical species
+  default_params_wdep <- list(so2 = c(0.000030, 0)
+                              so4 = c(0.000100, 0.000030),
+                              no = c(0.000029, 0),
+                              no2 = c(0.000510, 0),
+                              hno3 = c(0.000060, 0),
+                              no3 = c(0.000100, 0.000030),
+                              pm25 = c(0.000100, 0.000030)
+                              pm10 = c(0.000100, 0.000030))
+  
 }
