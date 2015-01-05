136c48d8ced4dc9ccd8fa13d6090ef82485602b5
rich-iannone <riannone@me.com>
2015-01-05 06:55:09 -0800

    Add 2 roxygen @param statements

--- R/calpuff_08_size_ddep_particles.R
+++ R/calpuff_08_size_ddep_particles.R
@@ -2,8 +2,8 @@
 #' @description This function validates and writes CALPUFF size parameters for dry deposition of particles.
 #' @param calpuff_inp the absolute path and filename for the working CALPUFF input file.
 #' @param species_names a vector list of species for which dry deposition size parameters will be supplied.
-#' @param species_geom_mass_mean_diam 
-#' @param species_geom_SD 
+#' @param species_geom_mass_mean_diam a vector list of geometric mass mean diameters (in micron units) for each of the species provided in the 'species_names' object.
+#' @param species_geom_SD a vector list of geometric standard deviation values (in micron units) for each of the species provided in the 'species_names' object.
 #' @export calpuff_08_size_ddep_particles
 
 calpuff_08_size_ddep_particles <- function(calpuff_inp = "calpuff_template.txt",
